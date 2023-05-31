485cb8c902c660f28e0a26b5bac5e64f4a3cdaa5
Yaguang Jia <jiayaguang@foxmail.com>
2023-05-31 20:05:11 +0800

    KE-41443 release 3.6.8-kyarthas-r1

--- bin/as.sh
+++ bin/as.sh
@@ -11,7 +11,7 @@
 #    date : 2023-04-19
 
 # current arthas script version
-ARTHAS_SCRIPT_VERSION=3.6.8
+ARTHAS_SCRIPT_VERSION=3.6.8-kyarthas-r1
 
 # SYNOPSIS
 #   rreadlink <fileOrDirPath>
@@ -455,7 +455,7 @@ EXAMPLES:
   ./as.sh --stat-url 'http://192.168.10.11:8080/api/stat'
   ./as.sh -c 'sysprop; thread' <pid>
   ./as.sh -f batch.as <pid>
-  ./as.sh --use-version 3.6.8
+  ./as.sh --use-version 3.6.8-kyarthas-r1
   ./as.sh --session-timeout 3600
   ./as.sh --attach-only
   ./as.sh --disabled-commands stop,dump
@@ -886,7 +886,7 @@ sanity_check() {
     if ([ "${OS_TYPE}" != "Linux" ] && [ "${OS_TYPE}" != "Mac" ]); then
         return
     fi
- 
+
     # 0 check whether the pid exist
     local pid=$(ps -p ${TARGET_PID} -o pid= 2>&1 )
 
